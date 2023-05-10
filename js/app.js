e75f269dc3ef2b80e9f5b076f8605d62f3bf70f7
bmtz <mtzsts1@gmail.com>
2023-05-10 14:08:35 -0600

    logica implementada

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 // Arreglo para guardar las referencias de los elementos del DOM
 const numeros = [];
-let resultado = "";
+let resultado = 0.0;
+let operaciones = [];
+let primerNumeroIngresado = false;
 
 // Obtener referencia a los elementos del DOM
 numeros[0] = document.getElementById("0");
@@ -15,31 +17,83 @@ numeros[8] = document.getElementById("8");
 numeros[9] = document.getElementById("9");
 numeros[10] = document.getElementById("c");
 numeros[11] = document.getElementById("u");
+numeros[12] = document.getElementById("div");
+numeros[13] = document.getElementById("multi");
+numeros[14] = document.getElementById("resta");
+numeros[15] = document.getElementById("suma");
+numeros[16] = document.getElementById(".");
+numeros[17] = document.getElementById("=");
+
 const pantalla = document.getElementById("resultado");
 
 // Agregando el evento a todos los botones
 for (let i = 0; i < 10; i++) {
-    numeros[i].addEventListener("click", function () {
-        mostrarEnPantalla(i.toString());
-    });
+  numeros[i].addEventListener("click", function () {
+    mostrarEnPantalla(i.toString());
+  });
 }
+
+numeros[16].addEventListener("click", function () {
+  mostrarEnPantalla(".");
+});
 
 //Funcion para mostrar en pantalla
 function mostrarEnPantalla(valor) {
-    if(resultado.length<12){
-        resultado += valor;
-    }
-    pantalla.value = resultado;
+  if (!primerNumeroIngresado && operaciones.length === 0 && valor !== ".") {
+    return; // Evitar agregar operadores sin ingresar números primero
+  }
+
+  if (valor === "." && operaciones.includes(".")) {
+    return; // Evitar agregar múltiples puntos decimales
+  }
+
+  if (operaciones.length < 12) {
+    operaciones.push(valor);
+  }
+  pantalla.value = operaciones.join("");
+  primerNumeroIngresado = true;
 }
 
 //Borra todos los numeros ingresados
 numeros[10].addEventListener("click", function () {
-    resultado = "";
-    pantalla.value = resultado;
+  operaciones = [];
+  resultado = 0.0;
+  pantalla.value = "";
 });
 
-//Brrar el ultmo numero ingresado
+//Borrar el último número ingresado
 numeros[11].addEventListener("click", function () {
-    resultado = resultado.slice(0, -1);
-    pantalla.value = resultado;
+  operaciones.pop();
+  pantalla.value = operaciones.join("");
+});
+
+//Divide los números
+numeros[12].addEventListener("click", function () {
+  operaciones.push("/");
+  pantalla.value = operaciones.join("");
+});
+
+//Multiplica los números
+numeros[13].addEventListener("click", function () {
+  operaciones.push("*");
+  pantalla.value = operaciones.join("");
+});
+
+//Resta los números
+numeros[14].addEventListener("click", function () {
+  operaciones.push("-");
+  pantalla.value = operaciones.join("");
+});
+
+//Suma los números
+numeros[15].addEventListener("click", function () {
+  operaciones.push("+");
+  pantalla.value = operaciones.join("");
+});
+
+//Muestra el resultado
+numeros[17].addEventListener("click", function () {
+  const expresion = operaciones.join("");
+  resultado = eval(expresion);
+  pantalla.value = resultado.toString();
 });
